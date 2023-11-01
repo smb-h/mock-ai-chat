00d2648dd86581f33b5ad881887c573f30a42558
SMBH <smbhop@gmail.com>
2023-11-01 14:04:15 +0100

    :zap: Fix(git): Running pre-commit

--- backend/app/app/api/api_v1/endpoints/login.py
+++ backend/app/app/api/api_v1/endpoints/login.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 from typing import Any
 
-from fastapi import APIRouter, Body, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
@@ -9,12 +9,14 @@ from app import crud, models, schemas
 from app.api import deps
 from app.core import security
 from app.core.config import settings
-from app.core.security import get_password_hash
-from app.utils import (
-    generate_password_reset_token,
-    send_reset_password_email,
-    verify_password_reset_token,
-)
+
+# from app.core.security import get_password_hash
+
+# from app.utils import (
+#     generate_password_reset_token,
+#     send_reset_password_email,
+#     verify_password_reset_token,
+# )
 
 router = APIRouter()
 
@@ -57,47 +59,49 @@ def test_token(
     return current_user
 
 
-@router.post("/password-recovery/{email}", response_model=schemas.Msg)
-def recover_password(email: str, db: Session = Depends(deps.get_db)) -> Any:
-    """
-    Password Recovery
-    """
-    user = crud.user.get_by_email(db, email=email)
+# @router.post("/password-recovery/{email}", response_model=schemas.Msg)
+# def recover_password(email: str, db: Session = Depends(deps.get_db)) -> Any:
+#     """
+#     Password Recovery
+#     """
+#     user = crud.user.get_by_email(db, email=email)
 
-    if not user:
-        raise HTTPException(
-            status_code=404,
-            detail="The user with this username does not exist in the system.",
-        )
-    password_reset_token = generate_password_reset_token(email=email)
-    send_reset_password_email(
-        email_to=user.email, email=email, token=password_reset_token
-    )
-    return {"msg": "Password recovery email sent"}
+#     if not user:
+#         raise HTTPException(
+#             status_code=404,
+#             detail=\
+#               "The user with this username does not exist in the system.",
+#         )
+#     password_reset_token = generate_password_reset_token(email=email)
+#     send_reset_password_email(
+#         email_to=user.email, email=email, token=password_reset_token
+#     )
+#     return {"msg": "Password recovery email sent"}
 
 
-@router.post("/reset-password/", response_model=schemas.Msg)
-def reset_password(
-    token: str = Body(...),
-    new_password: str = Body(...),
-    db: Session = Depends(deps.get_db),
-) -> Any:
-    """
-    Reset password
-    """
-    email = verify_password_reset_token(token)
-    if not email:
-        raise HTTPException(status_code=400, detail="Invalid token")
-    user = crud.user.get_by_email(db, email=email)
-    if not user:
-        raise HTTPException(
-            status_code=404,
-            detail="The user with this username does not exist in the system.",
-        )
-    elif not crud.user.is_active(user):
-        raise HTTPException(status_code=400, detail="Inactive user")
-    hashed_password = get_password_hash(new_password)
-    user.hashed_password = hashed_password
-    db.add(user)
-    db.commit()
-    return {"msg": "Password updated successfully"}
+# @router.post("/reset-password/", response_model=schemas.Msg)
+# def reset_password(
+#     token: str = Body(...),
+#     new_password: str = Body(...),
+#     db: Session = Depends(deps.get_db),
+# ) -> Any:
+#     """
+#     Reset password
+#     """
+#     email = verify_password_reset_token(token)
+#     if not email:
+#         raise HTTPException(status_code=400, detail="Invalid token")
+#     user = crud.user.get_by_email(db, email=email)
+#     if not user:
+#         raise HTTPException(
+#             status_code=404,
+#             detail=\
+#               "The user with this username does not exist in the system.",
+#         )
+#     elif not crud.user.is_active(user):
+#         raise HTTPException(status_code=400, detail="Inactive user")
+#     hashed_password = get_password_hash(new_password)
+#     user.hashed_password = hashed_password
+#     db.add(user)
+#     db.commit()
+#     return {"msg": "Password updated successfully"}
--- backend/app/app/api/api_v1/endpoints/users.py
+++ backend/app/app/api/api_v1/endpoints/users.py
@@ -8,7 +8,8 @@ from sqlalchemy.orm import Session
 from app import crud, models, schemas
 from app.api import deps
 from app.core.config import settings
-from app.utils import send_new_account_email
+
+# from app.utils import send_new_account_email
 
 router = APIRouter()
 
@@ -27,30 +28,31 @@ def read_users(
     return users
 
 
-@router.post("/", response_model=schemas.User)
-def create_user(
-    *,
-    db: Session = Depends(deps.get_db),
-    user_in: schemas.UserCreate,
-    current_user: models.User = Depends(deps.get_current_active_superuser),
-) -> Any:
-    """
-    Create new user.
-    """
-    user = crud.user.get_by_email(db, email=user_in.email)
-    if user:
-        raise HTTPException(
-            status_code=400,
-            detail="The user with this username already exists in the system.",
-        )
-    user = crud.user.create(db, obj_in=user_in)
-    if settings.EMAILS_ENABLED and user_in.email:
-        send_new_account_email(
-            email_to=user_in.email,
-            username=user_in.email,
-            password=user_in.password,
-        )
-    return user
+# @router.post("/", response_model=schemas.User)
+# def create_user(
+#     *,
+#     db: Session = Depends(deps.get_db),
+#     user_in: schemas.UserCreate,
+#     current_user: models.User = Depends(deps.get_current_active_superuser),
+# ) -> Any:
+#     """
+#     Create new user.
+#     """
+#     user = crud.user.get_by_email(db, email=user_in.email)
+#     if user:
+#         raise HTTPException(
+#             status_code=400,
+#             detail=\
+#               "The user with this username already exists in the system.",
+#         )
+#     user = crud.user.create(db, obj_in=user_in)
+#     if settings.EMAILS_ENABLED and user_in.email:
+#         send_new_account_email(
+#             email_to=user_in.email,
+#             username=user_in.email,
+#             password=user_in.password,
+#         )
+#     return user
 
 
 @router.put("/me", response_model=schemas.User)
